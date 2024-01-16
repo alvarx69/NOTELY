758fd15524198a948ce6189241a1ab66ff14137c
alvaro <alvaro03ha@gmail.com>
2024-01-16 12:59:32 +0100

    version1.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,19 @@
 @section('title', 'index')
 
 @section('content')
-    
+
 
     @if (Route::has('login'))
-    
-        {{-- @auth
-        <a href="{{ url('/dashboard') }}">Dashboard</a>
-        @else --}}
-        <div style="display: flex; flex-direction: row; justify-content: center;">
-            
-                <div style="margin-top: 20%; width: 60%; height: 10pc; border: 2px solid black; background-color: rgb(255, 255, 171); display: flex; justify-content: center; align-items: center; flex-direction: column">
-                    <h2 style="margin: 3px"><a href="{{ route('login') }}">Log in</a></h2>
-
-            {{-- @if (Route::has('register')) --}}
-                    <h2 style="margin: 30px"><a href="{{ route('register') }}">Register</a></h2>
-                </div>
-            
+        <div style=" font-family: Arial, Helvetica, sans-serif; text-align: center;">
+            <h2 style=" font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif">BIENVENID@ A NOTELY</h2>
+            <section>
+                <p style="margin-top: 5%;  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif">
+                    En notely podras crear notas rapidas y concisas o mas largas y especificas. <br>
+                    Esta aplicacion esta diseñada para organizarte el dia o la semana <br>
+                    Puedes apoyarnos dando una estrellita en <a href="https://github.com/alvarx69/NOTELY">GitHub</a> o seguirnos en <a href="https://www.instagram.com/_alvarx_/">Instagram</a>
+                </p>
+            </section>
         </div>
-        
-        {{-- @endif
-        @endauth --}}
-    
     @endif
-    
+
 @endsection
